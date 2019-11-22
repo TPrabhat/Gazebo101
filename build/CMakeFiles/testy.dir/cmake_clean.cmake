4c87e7bdf72cc83e24d8c95ce1df1fc6aa6a2cab
Prabhat Telidevara <prabhat.tp@gmail.com>
2019-11-22 09:03:06 -0800

    Initial commit

---
+++ build/CMakeFiles/testy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testy.dir/script/test_script.cpp.o"
+  "libtesty.pdb"
+  "libtesty.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
